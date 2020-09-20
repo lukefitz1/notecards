bbced2ff879bea6c8f6b2b504c7848fc373f0bb6
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2020-09-19 18:03:22 -0600

    Initial Commit

---
+++ notecards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notecards
+//
+//  Created by Luke Fitzgerald on 9/19/20.
+//  Copyright © 2020 Luke Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
